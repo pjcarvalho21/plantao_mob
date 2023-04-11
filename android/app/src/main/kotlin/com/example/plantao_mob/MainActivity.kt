c7b9b04608c218dfdd4f0288ba7f9ca6faa60d32
Paulo Carv41h0 <pjcarvalho21@gmail.com>
2023-04-11 13:15:20 -0300

    modificaoes config

---
+++ android/app/src/main/kotlin/com/example/plantao_mob/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.plantao_mob
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 
-main()=>runApp(const PlantaoApp());
+main() => runApp(const PlantaoApp());
 
 class PlantaoApp extends StatelessWidget {
-  const PlantaoApp({Key? key}) : super(key: key);  
-  
+  const PlantaoApp({Key? key}) : super(key: key);
+
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return const MaterialApp(home: MyHomePage());
-}
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     throw UnimplementedError();
   }
-
-  
 }
-
