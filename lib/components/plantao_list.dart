bf9b54e901eb1b7b8c7949050ac27fc6627b052a
Paulo Carv41h0 <pjcarvalho21@gmail.com>
2023-04-18 08:42:26 -0300

    filtro lista plantao

--- lib/components/plantao_list.dart
+++ lib/components/plantao_list.dart
@@ -4,18 +4,24 @@ import 'package:intl/intl.dart';
 
 class PlantaoList extends StatelessWidget {
   final List<Plantao> plantoes;
+
   const PlantaoList({super.key, required this.plantoes});
 
   @override
   Widget build(BuildContext context) {
+    List<Plantao> plantoesPrevistos = [];
+    plantoesPrevistos.addAll(plantoes);
+    plantoesPrevistos.retainWhere((plt) {
+      return plt.data.isAfter(DateTime.now());
+    });
     return SizedBox(
       height: 600,
-      child: plantoes.isEmpty
+      child: plantoesPrevistos.isEmpty
           ? Column()
           : ListView.builder(
-              itemCount: plantoes.length,
+              itemCount: plantoesPrevistos.length,
               itemBuilder: (ctx, index) {
-                final tr = plantoes[index];
+                final tr = plantoesPrevistos[index];
                 return Card(
                   elevation: 5,
                   margin: const EdgeInsets.symmetric(
@@ -23,17 +29,15 @@ class PlantaoList extends StatelessWidget {
                     horizontal: 5,
                   ),
                   child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: tr.data.isAfter(DateTime.now())
-                          ? Color.fromARGB(255, 177, 177, 177)
-                          : Color.fromARGB(255, 3, 3, 3),
+                    leading: const CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 177, 177, 177),
                       radius: 30,
                       child: Padding(
-                        padding: const EdgeInsets.all(6),
+                        padding: EdgeInsets.all(6),
                         child: FittedBox(
                           child: Text(
-                            tr.data.isAfter(DateTime.now()) ? 'PRV' : 'RLZ',
-                            style: const TextStyle(
+                            'PRV',
+                            style: TextStyle(
                               color: Colors.white,
                             ),
                           ),
